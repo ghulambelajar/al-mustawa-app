a98b1761142c4e8f719094bdf7e09a8e77a7e3b6
ghulambelajar <nilamsekuat@gmail.com>
2025-12-25 19:52:39 +0700

    add storage and change the way the server receives news data

--- server/routes/newRoutes.js
+++ server/routes/newRoutes.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const newsController = require("../controllers/newsController");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", newsController.getAllNews);
 router.get("/:id", newsController.getNewsById);
 
-router.post("/", newsController.createNews);
+router.post("/", upload.single("image"), newsController.createNews);
 router.delete("/:id", newsController.deleteNews);
 
 module.exports = router;
